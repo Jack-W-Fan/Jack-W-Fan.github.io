51053edfce1a4c2f8bccbf5d1672b2fef422e7d1
Jack Fan <108031686+Jack-W-Fan@users.noreply.github.com>
2024-07-20 09:52:32 -0700

    D3

--- script.js
+++ script.js
@@ -1,73 +1,75 @@
 const width = document.getElementById('visualization-container').clientWidth;
 const height = document.getElementById('visualization-container').clientHeight;
-
-// Create an SVG element
 const svg = d3.select("#visualization-container")
     .append("svg")
     .attr("width", width)
     .attr("height", height);
 
-// Example data (replace with your actual data)
-const data = [
+const dataScene1 = [
     {x: 10, y: 20},
     {x: 40, y: 90},
     {x: 80, y: 50}
 ];
+const dataScene2 = [
+    {x: 15, y: 30},
+    {x: 50, y: 70},
+    {x: 90, y: 40}
+];
+const dataScene3 = [
+    {x: 20, y: 10},
+    {x: 60, y: 80},
+    {x: 100, y: 60}
+];
 
-// Create scales
 const xScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.x)])
+    .domain([0, 100])
     .range([0, width]);
-
 const yScale = d3.scaleLinear()
-    .domain([0, d3.max(data, d => d.y)])
+    .domain([0, 100])
     .range([height, 0]);
 
-// Create circles for each data point
-svg.selectAll("circle")
-    .data(data)
-    .enter()
-    .append("circle")
-    .attr("cx", d => xScale(d.x))
-    .attr("cy", d => yScale(d.y))
-    .attr("r", 5)
-    .attr("fill", "blue");
-
-svg.append("text")
-    .attr("x", xScale(data[0].x))
-    .attr("y", yScale(data[0].y) - 10)
-    .attr("fill", "black")
-    .text("First point");
+function updateScene(data) {
+    svg.selectAll("*").remove();
+    svg.selectAll("circle")
+        .data(data)
+        .enter()
+        .append("circle")
+        .attr("cx", d => xScale(d.x))
+        .attr("cy", d => yScale(d.y))
+        .attr("r", 5)
+        .attr("fill", "blue");
+    svg.append("text")
+        .attr("x", xScale(data[0].x))
+        .attr("y", yScale(data[0].y) - 10)
+        .attr("fill", "black")
+        .text("Data Point 1");
+    svg.append("text")
+        .attr("x", xScale(data[1].x))
+        .attr("y", yScale(data[1].y) - 10)
+        .attr("fill", "black")
+        .text("Data Point 2");
+    svg.append("text")
+        .attr("x", xScale(data[2].x))
+        .attr("y", yScale(data[2].y) - 10)
+        .attr("fill", "black")
+        .text("Data Point 3");
+}
 
 let currentScene = 1;
-
 document.addEventListener("keydown", event => {
     if (event.key === "ArrowRight") {
         currentScene = (currentScene % 3) + 1;
-        updateScene(currentScene);
+        switch (currentScene) {
+            case 1:
+                updateScene(dataScene1);
+                break;
+            case 2:
+                updateScene(dataScene2);
+                break;
+            case 3:
+                updateScene(dataScene3);
+                break;
+        }
     }
 });
-
-function updateScene(scene) {
-    svg.selectAll("*").remove();
-    switch (scene) {
-        case 1:
-            svg.selectAll("circle")
-                .data(data)
-                .enter()
-                .append("circle")
-                .attr("cx", d => xScale(d.x))
-                .attr("cy", d => yScale(d.y))
-                .attr("r", 5)
-                .attr("fill", "blue");
-            break;
-        case 2:
-
-            break;
-        case 3:
-
-            break;
-    }
-}
-
-updateScene(currentScene);
+updateScene(dataScene1);
